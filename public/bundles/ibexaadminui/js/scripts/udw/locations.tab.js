f3a00367cd90f34be92e78547ef01e75ce9c3713
Ibexa Jenkins <jenkins@ibexa.co>
2022-02-01 22:08:46 +0000

    Template for v4.0.0

---
+++ public/bundles/ibexaadminui/js/scripts/udw/locations.tab.js
@@ -0,0 +1,41 @@
+(function(global, doc, ibexa, React, ReactDOM, Translator) {
+    const btns = doc.querySelectorAll('.ibexa-btn--udw-add');
+    const submitButton = doc.querySelector('#content_location_add_add');
+    const form = doc.querySelector('form[name="content_location_add"]');
+
+    if (!form) {
+        return;
+    }
+
+    const input = form.querySelector('#content_location_add_new_locations');
+    const udwContainer = doc.getElementById('react-udw');
+    const closeUDW = () => ReactDOM.unmountComponentAtNode(udwContainer);
+    const onConfirm = (items) => {
+        closeUDW();
+
+        input.value = items[0].id;
+        submitButton.click();
+    };
+    const onCancel = () => closeUDW();
+    const openUDW = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const config = JSON.parse(event.currentTarget.dataset.udwConfig);
+        const title = Translator.trans(/*@Desc("Select Location")*/ 'add_location.title', {}, 'universal_discovery_widget');
+
+        ReactDOM.render(
+            React.createElement(ibexa.modules.UniversalDiscovery, {
+                onConfirm,
+                onCancel,
+                containersOnly: true,
+                title,
+                multiple: false,
+                ...config,
+            }),
+            udwContainer
+        );
+    };
+
+    btns.forEach((btn) => btn.addEventListener('click', openUDW, false));
+})(window, window.document, window.ibexa, window.React, window.ReactDOM, window.Translator);
---
+++ public/bundles/ibexataxonomy/js/admin.taxonomy.tree.js
@@ -0,0 +1,36 @@
+(function(global, doc, React, ReactDOM, ibexa) {
+    const taxonomyTreeContainer = doc.querySelector('.ibexa-taxonomy-tree-container');
+
+    if (!taxonomyTreeContainer) {
+        return;
+    }
+
+    const token = doc.querySelector('meta[name="CSRF-Token"]').content;
+    const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
+    const taxonomyTreeRootElement = doc.querySelector('.ibexa-taxonomy-tree-container__root');
+    const { taxonomyName } = taxonomyTreeContainer.dataset;
+    const userId = ibexa.helpers.user.getId();
+    const removeTaxonomyTreeContainerWidth = (event) => {
+        if (event.detail.id !== 'ibexa-taxonomy-tree') {
+            return;
+        }
+
+        taxonomyTreeContainer.style.width = null;
+    };
+    const renderTree = () => {
+        const taxonomyName = taxonomyTreeContainer.dataset.taxonomy;
+
+        ReactDOM.render(
+            React.createElement(ibexa.modules.TaxonomyTree, {
+                userId,
+                taxonomyName,
+                restInfo: { token, siteaccess },
+                taxonomyName,
+            }),
+            taxonomyTreeRootElement,
+        );
+    };
+
+    doc.body.addEventListener('ibexa-tb-rendered', removeTaxonomyTreeContainerWidth, false);
+    renderTree();
+})(window, window.document, window.React, window.ReactDOM, window.ibexa);
