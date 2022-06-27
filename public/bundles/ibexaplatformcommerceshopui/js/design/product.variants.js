4812a8a417d2d75a9e29212b1cd1494d2befecf4
Ibexa Jenkins <jenkins@ibexa.co>
2022-06-27 12:29:08 +0000

    Template for v3.3.23

---
+++ public/bundles/ibexaplatformcommerceshopui/js/design/product.variants.js
@@ -0,0 +1,23 @@
+(function(global, doc, ibexa) {
+    class Product extends ibexa.eshop.widgets.ProductVariants {
+        updateContainer({ container, response }) {
+            const { priceData } = response.get_ProductInformation;
+
+            if (priceData.price) {
+                container.querySelector('[itemprop="price"]').innerHTML = priceData.price.formattedPrice;
+            } else {
+                container.querySelector('[itemprop="price"]').innerHTML = priceData.minPrice.formattedPrice;
+            }
+
+            return { container, response };
+        }
+    }
+
+    const product = new Product({
+        sku: doc.querySelector('input[name="ses_basket[0][sku]"]').value,
+    });
+    const accordion = new ibexa.eshop.widgets.Accordion();
+
+    product.init();
+    accordion.init('.ezcommerce-accordion--product');
+})(window, window.document, window.ibexa);
