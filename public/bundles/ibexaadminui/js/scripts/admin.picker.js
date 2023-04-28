0878de21bb70b68facdd63dbd07aa58b3156e88d
Ibexa Jenkins <jenkins@ibexa.co>
2023-04-28 14:19:18 +0000

    Template for v4.5.0

---
+++ public/bundles/ibexaadminui/js/scripts/admin.picker.js
@@ -0,0 +1,43 @@
+(function (global, doc, ibexa) {
+    const SELECTOR_PICKER = '.ibexa-picker';
+    const SELECTOR_PICKER_INPUT = '.ibexa-date-time-picker__input';
+    const SELECTOR_FORM_INPUT = '.ibexa-picker__form-input';
+    const pickers = doc.querySelectorAll(SELECTOR_PICKER);
+    const { formatShortDateTime } = ibexa.helpers.timezone;
+    const pickerConfig = {
+        enableTime: true,
+        time_24hr: true,
+        formatDate: (date) => formatShortDateTime(date, null),
+    };
+    const updateInputValue = (formInput, timestamp) => {
+        if (timestamp !== formInput.value) {
+            formInput.value = timestamp ?? '';
+
+            formInput.dispatchEvent(new Event('input'));
+        }
+    };
+    const initFlatPickr = (field) => {
+        const formInput = field.querySelector(SELECTOR_FORM_INPUT);
+        const pickerInput = field.querySelector(SELECTOR_PICKER_INPUT);
+        const customConfig = JSON.parse(pickerInput.dataset.flatpickrConfig || '{}');
+        let defaultDate;
+
+        if (formInput.value) {
+            defaultDate = new Date(formInput.value * 1000);
+        }
+
+        const dateTimePickerWidget = new ibexa.core.DateTimePicker({
+            container: field,
+            onChange: updateInputValue.bind(null, formInput),
+            flatpickrConfig: {
+                ...pickerConfig,
+                defaultDate,
+                ...customConfig,
+            },
+        });
+
+        dateTimePickerWidget.init();
+    };
+
+    pickers.forEach(initFlatPickr);
+})(window, window.document, window.ibexa, window.flatpickr);
---
+++ public/bundles/ibexaformbuilder/js/config-form/fields/field.name.js
@@ -0,0 +1,48 @@
+(function (global, doc, ibexa, Translator) {
+    let updateTimeout;
+    const ENTER_KEY_CODE = 13;
+    const input = doc.querySelector('#field_configuration_name');
+    const fieldId = doc.querySelector('#field_configuration_id').value.trim();
+    const unlockInput = () => (input.disabled = false);
+    const preventSettingName = () => {
+        const message = Translator.trans(
+            /*@Desc("Field name '%oldFieldName%' is already in use.")*/ 'type.different.field.name',
+            {
+                oldFieldName: input.value,
+            },
+            'form_builder',
+        );
+
+        ibexa.helpers.notification.showWarningNotification(message);
+
+        input.value = input.dataset.prevValue;
+        unlockInput();
+    };
+    const storePreviousName = () => (input.dataset.prevValue = input.value);
+    const updateFieldNameOnHitEnter = (event) => {
+        if (event.keyCode === ENTER_KEY_CODE) {
+            updateFieldName(event);
+        }
+    };
+    const updateFieldName = ({ currentTarget }) => {
+        global.clearTimeout(updateTimeout);
+
+        updateTimeout = global.setTimeout(() => {
+            const customEvent = new CustomEvent('ibexa-update-field-name', {
+                detail: {
+                    id: fieldId,
+                    name: currentTarget.value.trim(),
+                    successCallback: unlockInput,
+                    errorCallback: preventSettingName,
+                },
+            });
+
+            input.disabled = true;
+            doc.body.dispatchEvent(customEvent);
+        }, 200);
+    };
+
+    input.addEventListener('focus', storePreviousName, false);
+    input.addEventListener('blur', updateFieldName, false);
+    input.addEventListener('keydown', updateFieldNameOnHitEnter, false);
+})(window, window.document, window.ibexa, window.Translator);
