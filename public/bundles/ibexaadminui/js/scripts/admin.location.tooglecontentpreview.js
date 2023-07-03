496a869f691f8fd37797b5848cb11e2e54b16548
Ibexa Jenkins <jenkins@ibexa.co>
2023-07-03 15:02:22 +0000

    Template for v4.6.0

---
+++ public/bundles/ibexaadminui/js/scripts/admin.location.tooglecontentpreview.js
@@ -0,0 +1,26 @@
+(function (global, doc, localStorage, bootstrap) {
+    const CONTENT_PREVIEW_COLLAPSE_SELECTOR = '.ibexa-content-preview-collapse';
+    const DEFAULT_CONTENT_PREVIEW_TOGGLE_STATE_KEY = 'ibexa-content-preview-collapsed';
+    const getStateKey = (collapseTarget) => {
+        return collapseTarget.dataset.collapseStateKey || DEFAULT_CONTENT_PREVIEW_TOGGLE_STATE_KEY;
+    };
+    const getContentPreviewToggleState = (collapsable) => {
+        const stateKey = getStateKey(collapsable);
+        const value = localStorage.getItem(stateKey);
+
+        return !!JSON.parse(value);
+    };
+    const setContentPreviewToggleState = (event, value) => {
+        const stateKey = getStateKey(event.target);
+        localStorage.setItem(stateKey, value);
+    };
+
+    doc.querySelectorAll(CONTENT_PREVIEW_COLLAPSE_SELECTOR).forEach((collapsable) => {
+        new bootstrap.Collapse(collapsable, {
+            toggle: getContentPreviewToggleState(collapsable),
+        });
+
+        collapsable.addEventListener('hide.bs.collapse', (event) => setContentPreviewToggleState(event, true));
+        collapsable.addEventListener('show.bs.collapse', (event) => setContentPreviewToggleState(event, false));
+    });
+})(window, window.document, window.localStorage, window.bootstrap);
